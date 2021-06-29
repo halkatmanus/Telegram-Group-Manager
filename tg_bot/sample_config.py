197e18856f0ed0d4699a5790942a3fdf88ad266c
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-06-29 15:49:09 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = ""
-    OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "ViruZs"
+    API_KEY = "" # Get it from @BotFather
+    OWNER_ID = "" # Send /id command to @MissRose_bot
+    OWNER_USERNAME = "" # Your Telegram Username
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -26,14 +26,14 @@ class Config(object):
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
+    DONATION_LINK = "https://www.paypal.me/ssraut"  # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
+    BAN_STICKER = 'AgADaQADwZxgDA'  # Get sticker ID by sending your sticker to @LCxStickerID_bot
+    ALLOW_EXCL = True  # Allow ! commands as well as /
 
 
 class Production(Config):
